499b34f5ba12b69534e72e360a28be08a898818d
mukeshdutt <mukeshdutt5@gmail.com>
2017-03-20 01:57:33 +0530

    teams added

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core'
     selector: "my-app",
     template: `<h2>Welcome to Angular</h2>
             <h3>Version {{ version }}</h3>
+
+            <h4> Angular Teams </h4>            
+            <ul>
+            <li *ngFor="let member of team"> {{ member }} </li>
+            </ul>
     `
 })
 export class AppComponent {
-    version:string ="2.0"
+    
+    version:string ="2.0";
+    team = ["MIŠKO HEVERY", "IGOR MINAR", "NAOMI BLACK", "BRAD GREEN"];
 }
